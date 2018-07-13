6b65cecdf2137380b46b35b94215ca168831e35e
Ava Scherocman <31900548+avascherocman@users.noreply.github.com>
2018-07-13 10:31:17 -0400

    upload images

--- sketch.js
+++ sketch.js
@@ -1,8 +1,62 @@
-var player = document.getElementById("pig");
-function setup() {
-    createCanvas(640, 480);
+var isGameOver;
+var player;
+var playerImage;
+var enemy;
+var enemyImage;
+var backgroundImage;
+
+
+function preload() {
+  playerImage = loadImage("http://pngimg.com/uploads/frying_pan/frying_pan_PNG8358.png");
+  enemyImage = loadImage("pan.png");
+  backgroundImage = loadImage("farm.jpg");
 }
-  
+
+function setup() {
+  isGameOver = false;
+  createCanvas(256, 256);
+  player = createSprite(width / 2, height - (playerImage.height / 2), 0, 0);
+  player.addImage(playerImage);
+  enemy = createSprite(width / 2, 0, 0, 0);
+  enemy.addImage(enemyImage);
+  enemy.rotationSpeed = 4.0;
+}
+
 function draw() {
-  
+  if (isGameOver) {
+    gameOver();
+  } else {
+    if (enemy.overlap(player)) {
+      isGameOver = true;
+    }
+    background(backgroundImage);
+    if (keyDown(RIGHT_ARROW) && player.position.x < (width - (playerImage.width / 2))) {
+      player.position.x += 2;
+    }
+    if (keyDown(LEFT_ARROW) && player.position.x > (playerImage.width / 2)) {
+      player.position.x -= 2;
+    }
+    enemy.position.y = enemy.position.y + 3;
+    if (enemy.position.y > height) {
+      enemy.position.y = 0;
+      enemy.position.x = random(5, width - 5);
+    }
+    drawSprites();
+  }
+}
+
+function gameOver() {
+  background(0);
+  textAlign(CENTER);
+  fill("white");
+  text("Game Over!", width / 2, height / 2);
+  text("Click anywhere to try again", width / 2, 3 * height / 4);
+}
+
+function mouseClicked() {
+  isGameOver = false;
+  player.position.x = width / 2;
+  player.position.y = height - (playerImage.height / 2);
+  enemy.position.x = width / 2;
+  enemy.position.y = 0;
 }
